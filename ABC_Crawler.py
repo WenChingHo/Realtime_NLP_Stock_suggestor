b23dfccc1cbb36eb75792b4a5380d16c1ef35c4b
WenChingHo <85402171+WenChingHo@users.noreply.github.com>
2021-09-16 10:45:43 +0800

    Update ABC_Crawler.py

--- ABC_Crawler.py
+++ ABC_Crawler.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
-import threading
-import sqlite3
+from sqlalchemy import create_engine
+from django.conf import settings
+
+
 
 class Crawler(ABC):
     
@@ -8,13 +10,15 @@ class Crawler(ABC):
         "user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
         'Connection':'open',
     }
-    
-    def __init__(self ):
+
+    def __init__(self):
         pass
 
-    def connect_to_db(self, database):
-        self.connect = sqlite3.connect(database)
-        self.cursor = self.connect.cursor()    
+    @abstractmethod
+    def connect_to_db(self):
+        self.db_connection_url = "postgresql://eddie@localhost:5432/stocksuggestordb"
+        self.engine = create_engine(self.db_connection_url)
+        #df.to_sql("stockSuggestor_streamdata", engine, if_exists='replace', index=False, chunksize=10000)  
         
     @abstractmethod
     async def crawl_data_to_db(self):
